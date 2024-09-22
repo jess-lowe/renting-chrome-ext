bb2731f9fed1e869c6a49295d32f5c1ae904545b
jess-lowe <jlow337@aucklanduni.ac.nz>
2024-09-22 12:15:05 +1200

    feat: trying to clean the code up a little bit to be more modular

--- domain.js
+++ domain.js
@@ -12,11 +12,6 @@ let getHardLimit = new Promise((resolve) => {
   });
 });
 
-// if (window.location.href.includes("trademe")) {
-//   console.log("trademe");
-// } else if (window.location.href.includes("domain")) {
-//   console.log("domain");
-// }
 let isUpdating = false;
 let ideal;
 let hard_limit;
@@ -26,6 +21,12 @@ Promise.all([getIdeal, getHardLimit]).then(
     ideal = ideal_budget;
     hard_limit = hard_limit_budget;
     if (window.location.href.includes("trademe")) {
+      rent = document.getElementsByClassName(
+        "tm-property-search-card-price-attribute__price"
+      );
+      rooms = document.getElementsByName("bedroom");
+      let bedrooms = rooms.forEach((room) => room.nextSibling.innerText);
+
       console.log("trademe");
       const targetNode = document.getElementsByClassName(
         "tm-search-results__row"
@@ -33,12 +34,16 @@ Promise.all([getIdeal, getHardLimit]).then(
       const observer = new MutationObserver((mutations) => {
         mutations.forEach((mutation) => {
           if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
-            updatePrice(ideal, hard_limit);
+            if (!isUpdating) {
+              updatePrice(ideal, hard_limit);
+            }
           }
         });
       });
       const config = { childList: true, subtree: true };
       observer.observe(targetNode, config);
+
+      updateElements();
     } else if (window.location.href.includes("domain")) {
       var p = document.getElementsByClassName("css-mgq8yx");
       var r = document.getElementsByClassName("css-18biwo");
@@ -47,7 +52,7 @@ Promise.all([getIdeal, getHardLimit]).then(
         mutations.forEach((mutation) => {
           if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
             if (!isUpdating) {
-              updateElementsDomain();
+              updateElements(p, r);
             }
           }
         });
@@ -57,89 +62,22 @@ Promise.all([getIdeal, getHardLimit]).then(
         document.getElementsByClassName("css-8tedj6")[0],
         config
       );
-
-      // [
-      //   ...document.querySelectorAll(
-      //     '*[data-testId="listing-card-price"]:not(.done)'
-      //   ),
-      // ]
-      //   .map((item, index) => [
-      //     item,
-      //     document.querySelector(
-      //       `*[data-testid="results"] li:nth-child(${
-      //         index + 1
-      //       }) span[data-testid="property-features-feature"]:nth-child(1)`
-      //     ),
-      //   ])
-      //   .forEach(([price, rooms]) => {
-      //     console.log(rooms);
-      //     if (!rooms) {
-      //       rooms = 1;
-      //     } else {
-      //       rooms = parseInt((rooms = rooms.children[0].innerText[0])) || 1;
-      //     }
-
-      //     updatePriceDomain(ideal, hard_limit, price, rooms);
-
-      //     console.log(price);
-      //     console.log(rooms);
-      //     price.classList.add("done");
-      //   });
       if (p.length != r.length) {
         console.log("x and y not equal");
       }
-
-      const elements = [...document.getElementsByClassName("css-mgq8yx")]
-        .map((item, index) => [
-          item,
-          // document.querySelector(
-          //   `*[data-testid="results"] li:nth-child(${index}) span[data-testid="property-features-feature"]:nth-child(1)`
-          // ),
-          document.getElementsByClassName("css-18biwo")[index],
-        ])
-        .forEach(([price, rooms]) => {
-          // console.log(price);
-          // console.log(rooms);
-          console.log(price, rooms);
-          if (!price.classList.contains("done")) {
-            if (!rooms) {
-              rooms = 1;
-            } else {
-              rooms = parseInt(rooms.children[0].innerText[0]) || 1;
-            }
-
-            updatePriceDomain(ideal, hard_limit, price, rooms);
-
-            //   console.log(price);
-            //   console.log(rooms);
-            price.classList.add("done");
-          }
-        });
-
-      console.log("domain");
-
-      //   console.log(targetNode);
-      //   observer.observe(targetNode, config);
+      updateElements(p, r);
     }
   }
 );
 
-function updateElementsDomain() {
+function updateElements(pricesElements, roomsElements) {
   if (isUpdating) {
     return;
   }
   isUpdating = true;
-  const elements = [...document.getElementsByClassName("css-mgq8yx")]
-    .map((item, index) => [
-      item,
-      // document.querySelector(
-      //   `*[data-testid="results"] li:nth-child(${index}) span[data-testid="property-features-feature"]:nth-child(1)`
-      // ),
-      document.getElementsByClassName("css-18biwo")[index],
-    ])
+  const elements = [...pricesElements]
+    .map((item, index) => [item, roomsElements[index]])
     .forEach(([price, rooms]) => {
-      // console.log(price);
-      // console.log(rooms);
       console.log(price, rooms);
       if (!price.classList.contains("done")) {
         if (!rooms) {
@@ -147,43 +85,54 @@ function updateElementsDomain() {
         } else {
           rooms = parseInt(rooms.children[0].innerText[0]) || 1;
         }
-
-        updatePriceDomain(ideal, hard_limit, price, rooms);
-
-        //   console.log(price);
-        //   console.log(rooms);
+        updatePrice(ideal, hard_limit, price, rooms);
         price.classList.add("done");
       }
     });
   isUpdating = false;
 }
 
-// let previousUrl = window.location.href;
-
-// setInterval(() => {
-//   const currentUrl = window.location.href;
-//   if (currentUrl !== previousUrl) {
-//     updatePrice(ideal, hard_limit);
-//     previousUrl = currentUrl;
-//   }
-// }, 5000); // Check every 5 seconds
-function updatePrice(ideal, hard_limit) {
+function updatePrice(ideal, hard_limit, priceElement, rooms) {
+  ideal = parseInt(ideal);
+  hard_limit = parseInt(hard_limit);
   if (window.location.href.includes("trademe")) {
-    updatePriceTradeMe(ideal, hard_limit);
+    updatePriceTradeMe(ideal, hard_limit, priceElement, rooms);
   } else if (window.location.href.includes("domain")) {
-    updatePriceDomain(ideal, hard_limit);
+    updatePriceDomain(ideal, hard_limit, priceElement, rooms);
   }
 }
 
 function updatePriceDomain(ideal, hard_limit, priceElement, rooms) {
   ideal = parseInt(ideal);
   hard_limit = parseInt(hard_limit);
-  var price = priceElement.innerHTML;
-  var originalPrice = price;
-  var bedrooms = rooms;
+  let price = priceElement.innerHTML;
+  let bedrooms = rooms;
+
   if (bedrooms == 0 || bedrooms == 1) {
     bedrooms = 1;
   }
+  price = parsePriceDomain(price);
+  if (price.match(/[0-9]/g)) {
+    price = parseInt(price);
+    price_per_bedroom = parseInt((price / bedrooms).toFixed(0));
+    var styledPPB = '<em style="color:purple">$' + price_per_bedroom;
+
+    if (price_per_bedroom >= hard_limit) {
+      styledPPB = '<em style="color:red">$' + price_per_bedroom;
+      //hide the ones over budget;
+      // a = document.getElementsByClassName("css-1qp9106")[i].style.display =
+      //   "none";
+    } else if (price_per_bedroom < hard_limit && price_per_bedroom >= ideal) {
+      styledPPB = '<em style="color:orange">$' + price_per_bedroom;
+    } else if (price_per_bedroom < ideal) {
+      styledPPB = '<em style="color:green">$' + price_per_bedroom;
+    }
+    priceElement.innerHTML =
+      styledPPB + "pw per bedroom </em> - " + priceElement.innerHTML;
+  }
+}
+
+function parsePriceDomain(price) {
   if (price.includes(",")) {
     price = price.replace(",", "");
   }
@@ -212,90 +161,8 @@ function updatePriceDomain(ideal, hard_limit, priceElement, rooms) {
       price = price.split("<")[0];
     }
   }
-  if (!price.match(/[0-9]/g)) {
-    price = "No price";
-  } else {
-    price = parseInt(price);
-    price_per_bedroom = parseInt((price / bedrooms).toFixed(0));
-    var styledPPB = '<em style="color:purple">$' + price_per_bedroom;
 
-    if (price_per_bedroom >= hard_limit) {
-      styledPPB = '<em style="color:red">$' + price_per_bedroom;
-      // x.getParentNode().style.display = "none";
-      // a = document.getElementsByClassName("css-1qp9106")[i].style.display =
-      //   "none";
-    } else if (price_per_bedroom < hard_limit && price_per_bedroom >= ideal) {
-      styledPPB = '<em style="color:orange">$' + price_per_bedroom;
-    } else if (price_per_bedroom < ideal) {
-      styledPPB = '<em style="color:green">$' + price_per_bedroom;
-    }
-    priceElement.innerHTML =
-      styledPPB + "pw per bedroom </em> - " + priceElement.innerHTML;
-  }
-}
-
-function updatePriceDomain2(ideal, hard_limit) {
-  // Rest of your code here
-  ideal = parseInt(ideal);
-  hard_limit = parseInt(hard_limit);
-  var x = document.getElementsByClassName("css-mgq8yx");
-  var y = document.getElementsByClassName("css-18biwo");
-
-  var z = [];
-  for (i = 0; i < y.length; i++) {
-    z.push(y[i].children[0].innerText[0]);
-  }
-
-  for (i = 0; i < x.length; i++) {
-    var price = x[i].innerHTML;
-    var bedrooms = parseInt(z[i]);
-    if (price.includes(",")) {
-      price = price.replace(",", "");
-    }
-    price.toLowerCase();
-
-    if (price.includes("single")) {
-      price = "single bedroom";
-    }
-    //find just the numbers including decimals
-    // price = price.match(/[0-9]+/g);
-
-    if (price.includes("$")) {
-      price = price.split("$")[1];
-    }
-
-    if (price.includes("p")) {
-      price = price.split("p")[0];
-    }
-    //if price has no numbers
-
-    if (price.includes(" ")) {
-      price = price.split(" ")[0];
-    }
-
-    if (price.includes("<")) {
-      price = price.split("<")[0];
-    }
-    if (!price.match(/[0-9]/g)) {
-      price = "No price";
-    } else {
-      price = parseInt(price);
-      price_per_bedroom = parseInt((price / bedrooms).toFixed(0));
-      var styledPPB = '<em style="color:purple">$' + price_per_bedroom;
-
-      if (price_per_bedroom >= hard_limit) {
-        styledPPB = '<em style="color:red">$' + price_per_bedroom;
-        // x.getParentNode().style.display = "none";
-        a = document.getElementsByClassName("css-1qp9106")[i].style.display =
-          "none";
-      } else if (price_per_bedroom < hard_limit && price_per_bedroom >= ideal) {
-        styledPPB = '<em style="color:orange">$' + price_per_bedroom;
-      } else if (price_per_bedroom < ideal) {
-        styledPPB = '<em style="color:green">$' + price_per_bedroom;
-      }
-      x[i].innerHTML = styledPPB + "pw per bedroom </em> - " + x[i].innerHTML;
-    }
-  }
+  return price;
 }
 
 function updatePriceTradeMe(ideal, hard_limit) {
@@ -309,6 +176,7 @@ function updatePriceTradeMe(ideal, hard_limit) {
 
   for (i = 0; i < rent.length; i++) {
     var price = rent[i].innerHTML;
+
     var bedrooms = parseInt(rooms[i].nextSibling.getInnerHTML());
 
     price = price.split(" ")[0];
